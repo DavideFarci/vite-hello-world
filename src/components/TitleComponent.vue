f893ed7c79e3b4ab58cd5edf92982689f37f395c
DavideFarci <yank92.df@gmail.com>
2023-05-17 16:55:52 +0200

    fix title

--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <h1>ciao a tutti</h1>
+    <h1>La mia prima app con Vite!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam eius veritatis accusantium maxime? Totam officiis sit ut voluptatum ducimus illum necessitatibus quasi reprehenderit quo, excepturi quod maxime illo, quam suscipit!</p>
 </template>
 
