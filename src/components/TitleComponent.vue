5767b87234fc61e91aaddee34e86a3c4942f497a
DavideFarci <yank92.df@gmail.com>
2023-05-17 16:54:50 +0200

    add new component TitleComponent

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<template>
+    <h1>ciao a tutti</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam eius veritatis accusantium maxime? Totam officiis sit ut voluptatum ducimus illum necessitatibus quasi reprehenderit quo, excepturi quod maxime illo, quam suscipit!</p>
+</template>
+
+<style scoped>
+
+</style>
